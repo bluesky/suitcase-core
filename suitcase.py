75f12c94b8ce3f12faea77437583b2b7ead97613
Thomas A Caswell <tcaswell@bnl.gov>
2015-09-23 12:11:47 -0400

    MNT: fix up from mds changes
     - Deal with new header documents
     - deal with docs being read-only

--- suitcase.py
+++ suitcase.py
@@ -1,10 +1,7 @@
-import copy
-from collections import MutableMapping
+from collections import Mapping
 import warnings
 import h5py
 import json
-import numpy as np
-import metadatastore
 from metadatastore.commands import find_events
 from dataportal.broker.simple_broker import fill_event
 
@@ -20,48 +17,53 @@ def export(headers, filename):
     """
     with h5py.File(filename) as f:
         for header in headers:
-            assert isinstance(header, MutableMapping)
-            header = copy.deepcopy(header)
+            header = dict(header)
             try:
-                descriptors = header.pop('event_descriptors')
+                descriptors = header.pop('descriptors')
             except KeyError:
                 warnings.warn("Header with uid {header.uid} contains no "
                               "data.".format(header), UserWarning)
                 continue
-            top_group_name = repr(header).replace(' ', '_')[1:-1]
+            top_group_name = header['start']['uid']
             group = f.create_group(top_group_name)
             _safe_attrs_assignment(group, header)
             for i, descriptor in enumerate(descriptors):
-                desc_group = group.create_group('Event_Stream_{0}'.format(i))
+                # make sure it's a dictionary and trim any spurious keys
+                descriptor = dict(descriptor)
+                descriptor.pop('_name', None)
+
+                desc_group = group.create_group(descriptor['uid'])
+
                 data_keys = descriptor.pop('data_keys')
                 _safe_attrs_assignment(desc_group, descriptor)
+
                 events = list(find_events(descriptor=descriptor))
                 event_times = [e['time'] for e in events]
-                desc_group.create_dataset('event_times', event_times)
+                desc_group.create_dataset('time', data=event_times)
                 data_group = desc_group.create_group('data')
                 ts_group = desc_group.create_group('timestamps')
                 [fill_event(e) for e in events]
                 for key, value in data_keys.items():
                     print('data key = %s' % key)
+                    value = dict(value)
                     timestamps = [e['timestamps'][key] for e in events]
                     ts_group.create_dataset(key, data=timestamps)
                     data = [e['data'][key] for e in events]
                     dataset = data_group.create_dataset(key, data=data)
                     # Put contents of this data key (source, etc.)
                     # into an attribute on the associated data set.
-                    # value['data_broker_shape'] = value.pop('shape')
-                    # value['data_broker_dtype'] = value.pop('dtype')
-                    # _safe_attrs_assignment(dataset, dict(value))
+                    _safe_attrs_assignment(dataset, dict(value))
 
 
 def _clean_dict(d):
+    d = dict(d)
     for k, v in list(d.items()):
         # Store dictionaries as JSON strings.
-        if isinstance(v, MutableMapping):
+        if isinstance(v, Mapping):
             d[k] = _clean_dict(d[k])
             continue
         try:
-            json.dumps(v)
+            d[k] = json.dumps(v)
         except TypeError:
             d[k] = str(v)
     return d
