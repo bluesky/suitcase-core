5c7a3c7b6d9eacbd6066fc4458cff28d68cc3518
licode <lili@bnl.gov>
2018-01-18 13:39:39 -0500

    TST: clean database time

--- suitcase/tests/conftest.py
+++ suitcase/tests/conftest.py
@@ -7,38 +7,35 @@ import sys
 if sys.version_info >= (3, 5):
     from bluesky.tests.conftest import RE, hw
 
-
 AUTH = os.environ.get('MDSTESTWITHAUTH', False)
-
-db_name1 = str(uuid.uuid4())
-db_name2 = str(uuid.uuid4())
-
-test_config = {
-    'metadatastore': {
-        'module': 'databroker.headersource.mongo',
-        'class': 'MDS',
-        'config': {
-            'host': 'localhost',
-            'port': 27017,
-            'database': db_name1,
-            'timezone': 'US/Eastern'}
-    },
-    'assets': {
-        'module': 'databroker.assets.mongo',
-        'class': 'Registry',
-        'config': {
-            'host': 'localhost',
-            'port': 27017,
-            'database': db_name2}
-    }
-}
-
 
 @pytest.fixture(params=[1], scope='function')
 def db_all(request):
     '''Provide a function level scoped metadatastore instance talking to
     temporary database on localhost:27017 with focus on v1.
     '''
+    db_name1 = "mds_testing_disposable_{}".format(str(uuid.uuid4()))
+    db_name2 = "mds_testing_disposable_{}".format(str(uuid.uuid4()))
+
+    test_config = {
+        'metadatastore': {
+            'module': 'databroker.headersource.mongo',
+            'class': 'MDS',
+            'config': {
+                'host': 'localhost',
+                'port': 27017,
+                'database': db_name1,
+                'timezone': 'US/Eastern'}
+        },
+        'assets': {
+            'module': 'databroker.assets.mongo',
+            'class': 'Registry',
+            'config': {
+                'host': 'localhost',
+                'port': 27017,
+                'database': db_name2}
+        }
+    }
     db = Broker.from_config(test_config)
 
     def delete_dm():
