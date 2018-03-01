91f414b071c72b13b520b0328cd027f384fde171
Julien R M Lhermitte <jrmlhermitte@bnl.gov>
2018-03-01 14:23:36 -0500

    Change keys in data

--- suitcase/iss_series.py
+++ suitcase/iss_series.py
@@ -6,23 +6,43 @@ import numpy as np
 from databroker import Broker, temp_config
 
 
-def parse_series_file(fname, prefix=None, **kwargs):
+def make_header(fname, prefix=None, suffix=None):
+    ''' Make a dummy header from file if one not supplied.'''
     if prefix is None:
         prefix = ""
+    if suffix is None:
+        suffix = ""
+
+    # quickly load (files assumed not to be large)
+    data = np.loadtxt(fname).T
+    numcols = len(data[:,0])-1
+
+    header = [f"{prefix}{i}{suffix}" for i in range(numcols)]
+    header = ["energy"] + header
+
+    return header
+
+
+def parse_series_file(fname, header=None, **kwargs):
+    ''' Parse a series file which is assumed to be
+        energies mu1 mu2 ....
+    '''
+    if header is None:
+        header = make_header(fname)
 
     res = np.loadtxt(fname, **kwargs).T
+
     energies = res[0]
     data = res[1:]
     data_dict = dict()
 
-    for i in range(len(data)):
-        key = prefix + f"{i}"
-        data_dict[key] = data[i]
+    for i, name in enumerate(header[1:]):
+        data_dict[name] = data[i]
 
     return energies, data_dict
 
 
-def ingest(fname, headerprefix=None, md=None):
+def ingest(fname, header=None, md=None):
     ''' Ingest Series file.
         This file is an ascii table of Energy, ...
         where the remaining values are from samples.
@@ -35,15 +55,15 @@ def ingest(fname, headerprefix=None, md=None):
         md : dict, optional
             additional metadata to add (author name? etc)
 
-        headerprefix : str
-            prefix for the columns of the series file
+        header: str
+            header for the columns
+            (should be energy, mu1 ....)
 
         Returns
         -------
             Generator of documents to be inserted into databroker
-
     '''
-    energies, data_dict = parse_series_file(fname, prefix=headerprefix)
+    energies, data_dict = parse_series_file(fname, header=header)
 
     for data_key, data in data_dict.items():
         doc_time = time.time()
@@ -60,7 +80,7 @@ def ingest(fname, headerprefix=None, md=None):
                 'name': 'primary',
                 'data_keys': {'energy' : {'dtype': 'number', 'shape': None, 'source':
                                    'Series'},
-                              data_key: {'dtype': 'number', 'shape': None, 'source':
+                              data_key : {'dtype': 'number', 'shape': None, 'source':
                                    'Series'}
                              }
                }
